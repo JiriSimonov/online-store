3deca5334592cb6cc33efd76b4c80e1c0d55629e
JiriSimonov <jirisimonov@gmail.com>
2022-12-02 19:24:32 +0300

    refactor: router

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,21 +1,15 @@
 import { RoutesObj } from "../interfaces/interfaces";
 
 export class Router {
-  constructor(
-    private readonly routes: RoutesObj,
-    private errorCallback: () => void,
-  ) {
+  
+  constructor(private readonly routes: RoutesObj, private errorCallback: () => void) {
     window.addEventListener('hashchange', this.onHashChangeHandler);
     this.onHashChangeHandler();
   }
 
   onHashChangeHandler = () => {
     const hashPath = window.location.hash.slice(1);
-    if (Object.keys(this.routes).includes(hashPath)) {
-      this.routes[hashPath]();
-    } else {
-      this.errorCallback();
-    }
+    Object.keys(this.routes).includes(hashPath) ? this.routes[hashPath]() :this.errorCallback();
   };
 
   destroy() {
