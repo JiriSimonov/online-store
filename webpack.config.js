129346e527ec0fa16533026c60278a8a26853a72
EternalRival <eternalrival8@gmail.com>
2022-12-06 11:32:48 +0300

    ci: add webp to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ const config = {
         use: [stylesHandler, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
         type: 'asset',
       },
 
