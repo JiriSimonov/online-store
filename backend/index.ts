0785def25e0012c44b341e8312d2d537becc52e6
EternalRival <eternalrival8@gmail.com>
2022-12-06 11:14:27 +0300

    feat: add 527 keyboard images

--- backend/index.ts
+++ backend/index.ts
@@ -3,11 +3,10 @@ import { downloadImages, generateKeyboardJSON } from './keyboards-json/index';
 
 generateKeyboardJSON(path.resolve('src', 'data', 'keyboards.json'));
 
-downloadImages(path.resolve('src', 'assets', 'images', 'keyboards')).then((promises) => {
+downloadImages(/* path.resolve('src', 'assets', 'images', 'keyboards') */).then((promises) => {
   const rejected = promises
     .filter((promise): promise is PromiseRejectedResult => promise.status === 'rejected')
     .map((v) => `${v.reason}`.slice('Error: '.length));
-
   if (rejected.length) console.log('Failed downloads (Bad URL):', rejected);
-  else console.log('All images downloaded!');
+  else console.log(`All images downloaded! (${promises.length})`);
 });
