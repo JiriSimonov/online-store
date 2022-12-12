06e8d0330dbc4bef70357fab1b0db543ced8ed03
JiriSimonov <jirisimonov@gmail.com>
2022-12-12 23:06:34 +0300

    feat: add filtres

--- src/components/filters/features-filter.ts
+++ src/components/filters/features-filter.ts
@@ -1,36 +1,23 @@
 import BaseComponent from '../elements/base-component';
 import Filter from './filter';
 import Button from '../elements/button';
+import ProductsListState from '../../states/goods-state';
+
+const btns = ['С русскими буквами', 'Программируемая', 'С подсветкой', 'HOT-SWAP', 'Мультимедиа-клавиши', 'Специально под Mac', 'Беспроводная', 'Эргономическая'];
 
 export default class FeaturesFilter extends Filter {
   private filterWrapper: BaseComponent;
 
-  private ru: Button;
+  private buttons: Button[];
 
-  private prog: Button;
-
-  private light: Button;
-
-  private hotswap: Button;
-
-  private multi: Button;
-
-  private formac: Button;
-
-  private bezprovodov: Button;
-
-  private ergo: Button;
-
-  constructor() {
-    super('Наличие');
+  constructor(private productsState: ProductsListState) {
+    super('Фичи');
     this.filterWrapper = new BaseComponent({ className: 'filter__wrapper', parent: this.node });
-    this.ru = new Button({ className: 'filter__btn', text: 'С русскими буквами', parent: this.filterWrapper.getNode() });
-    this.prog = new Button({ className: 'filter__btn', text: 'Программируемая', parent: this.filterWrapper.getNode() });
-    this.light = new Button({ className: 'filter__btn', text: 'С подсветкой', parent: this.filterWrapper.getNode() });
-    this.hotswap = new Button({ className: 'filter__btn', text: 'HOT-SWAP', parent: this.filterWrapper.getNode() });
-    this.multi = new Button({ className: 'filter__btn', text: 'Мультимедиа-клавиши', parent: this.filterWrapper.getNode() });
-    this.formac = new Button({ className: 'filter__btn', text: 'Специально под Mac', parent: this.filterWrapper.getNode() });
-    this.bezprovodov = new Button({ className: 'filter__btn', text: 'Беспроводная', parent: this.filterWrapper.getNode() });
-    this.ergo = new Button({ className: 'filter__btn', text: 'Эргономическая', parent: this.filterWrapper.getNode() });
+    this.buttons = btns.map((item) => new Button({ className: 'filter__btn', text: item, parent: this.filterWrapper.getNode() }));
+    this.buttons.map((item) => item.getNode().addEventListener('click', () => {
+      this.buttons.map((elem) => elem.getNode().classList.remove('active'));
+      item.getNode().classList.toggle('active');
+      productsState.set({ features: item.getNode().textContent as string });
+    }));
   }
 }
