3707595fe285ac176516c96f36904e8e8c4313d5
EternalRival <eternalrival8@gmail.com>
2022-12-15 10:15:25 +0300

    ci: reconfigure project

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
+
+const getTemplate = (title, lang) => `<!DOCTYPE html>
+<html lang="${lang ?? 'en'}">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+  </head>
+  <body>
+    <div class="root" id="root"></div>
+  </body>
+</html>`;
+
+const config = {
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  devServer: {
+    static: { directory: path.resolve('dist') },
+    open: { app: { name: 'chrome', arguments: ['--incognito'] } },
+    host: 'localhost',
+    hot: true,
+    compress: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: getTemplate('Keyboards Store', 'en'),
+      fileName: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.(ts|tsx)$/i, loader: 'ts-loader', exclude: ['/node_modules/'] },
+      { test: /\.css$/i, use: [stylesHandler, 'css-loader'] },
+      { test: /\.s[ac]ss$/i, use: [stylesHandler, 'css-loader', 'sass-loader'] },
+      { test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i, type: 'asset' },
+    ],
+  },
+  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js', '...'] },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+
+    config.plugins.push(new MiniCssExtractPlugin());
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
