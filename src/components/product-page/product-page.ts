8889e1f3f8ff439a21c419b91e5347fc07c955f9
JiriSimonov <jirisimonov@gmail.com>
2022-12-17 12:04:47 +0300

    feat: add back to store button

--- src/components/product-page/product-page.ts
+++ src/components/product-page/product-page.ts
@@ -15,7 +15,7 @@ export class ProductPage extends BaseComponent {
     super({ className: 'container store product'});
     this.card = new ProductCard(keyboard);
     this.appendEl(this.card);
-    this.backBtn = new Button({ className: 'cart__btn', text: 'Назад', parent: this.node });
+    this.backBtn = new Button({ className: 'cart__btn', text: 'Назад', parent: this.node, onclick: () => window.location.hash = '#store' });
     this.title = new BaseComponent({ tag: 'h2', className: 'product__title', text: 'Характеристики', parent: this.card.getNode() });
     this.descrFields = Object.entries(keyboard.properties).map((item) => new DescriptionField({key: item[0], value: item[1].join('\n')}));
     this.descrList = new BaseComponent({ tag: 'ul', className: 'product__list', parent: this.card.getNode() });
