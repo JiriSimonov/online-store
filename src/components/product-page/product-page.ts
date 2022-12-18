18697db33d6f1f73562e80bd591d77c8b98c9682
JiriSimonov <jirisimonov@gmail.com>
2022-12-18 06:22:15 +0300

    feat: add go to cart button

--- src/components/product-page/product-page.ts
+++ src/components/product-page/product-page.ts
@@ -1,3 +1,4 @@
+import { DB } from './../../services/db/db';
 import { Button } from './../elements/button';
 import { DescriptionField } from './../elements/description-field';
 import { Keyboard } from '../../services/db/keyboard';
@@ -5,22 +6,35 @@ import { ProductCard } from './../product/product-card';
 import { BaseComponent } from '../elements/base-component';
 
 export class ProductPage extends BaseComponent {
+  private container: BaseComponent;
+
   private card: ProductCard;
+
   private descrFields: DescriptionField[];
+
   private descrList: BaseComponent;
+
   private title: BaseComponent;
+
+  private btnWrapper: BaseComponent;
+
   private backBtn: Button;
 
+  private cartBtn: Button;
+
   constructor(keyboard: Keyboard) {
-    super({ className: 'container store product' });
+    super({ className: 'container' });
+    this.container = new BaseComponent({ className: 'store product', parent: this.node });
     this.card = new ProductCard(keyboard);
-    this.appendEl(this.card);
+    this.container.appendEl(this.card);
+    this.btnWrapper = new BaseComponent({ className: 'product__wrapper', parent: this.node });
     this.backBtn = new Button({
       className: 'cart__btn',
       text: 'Назад',
-      parent: this.node,
+      parent: this.btnWrapper.getNode(),
       onclick: () => (window.location.hash = '#store'),
     });
+    this.cartBtn = new Button({  className: 'cart__btn', text: 'Оформить заказ', onclick: () => window.location.hash = '#cart' });
     this.title = new BaseComponent({
       tag: 'h2',
       className: 'product__title',
@@ -35,6 +49,7 @@ export class ProductPage extends BaseComponent {
       className: 'product__list',
       parent: this.card.getNode(),
     });
+    if(DB.cart.some((item) => item[0] === keyboard)) this.btnWrapper.appendEl(this.cartBtn); // TODO прокинуть эммитер сюда, что бы появлялась кнопка перейти в корзину
     this.descrList.appendEl(this.descrFields);
   }
 }
