a612955359071dd1300e719e9395021089709aa7
JiriSimonov <jirisimonov@gmail.com>
2022-12-18 05:50:58 +0300

    feat: add custom cursor

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const getTemplate = (title, lang) => `<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
   </head>
-  <body>
+  <body class="body">
     <div class="root" id="root"></div>
   </body>
 </html>`;
