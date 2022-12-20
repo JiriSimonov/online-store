bc58e631fea7b19bd45b004b23587af1c9a6638d
EternalRival <eternalrival8@gmail.com>
2022-12-21 01:43:56 +0300

    ci: split webpack prod and dev. add sourcemaps

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,11 @@
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
-
-const getTemplate = (title) => `<!DOCTYPE html>
-<html lang="ru-RU">
+const getTemplate = (title, lang) => `<!DOCTYPE html>
+<html lang="${lang}">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,24 +17,21 @@ const getTemplate = (title) => `<!DOCTYPE html>
   </body>
 </html>`;
 
-const config = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src', 'index.ts'),
+  },
+
   output: {
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
     assetModuleFilename: 'assets/[name][ext]',
+    clean: true,
   },
-  devServer: {
-    static: { directory: path.resolve('dist') },
-    open: { app: { name: 'chrome', arguments: ['--incognito'] } },
-    host: 'localhost',
-    hot: true,
-    compress: true,
-  },
+
   plugins: [
     new ESLintPlugin({ extensions: ['ts'] }),
     new HtmlWebpackPlugin({
-      templateContent: getTemplate('Keyboards Store'),
+      templateContent: getTemplate('Keyboards Store', 'ru-RU'),
       fileName: 'index.html',
     }),
     new CopyPlugin({
@@ -48,24 +41,15 @@ const config = {
       ],
     }),
   ],
+
   module: {
     rules: [
       { test: /\.(ts|tsx)$/i, loader: 'ts-loader', exclude: ['/node_modules/'] },
-      { test: /\.css$/i, use: [stylesHandler, 'css-loader'] },
-      { test: /\.s[ac]ss$/i, use: [stylesHandler, 'css-loader', 'sass-loader'] },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader'] },
       { test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i, type: 'asset' },
     ],
   },
+
   resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js', '...'] },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-
-    config.plugins.push(new MiniCssExtractPlugin());
-  } else {
-    config.mode = 'development';
-  }
-  return config;
 };
