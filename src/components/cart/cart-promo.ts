e6a2ce50de207d71671aa1ba11920ccf0506bab4
JiriSimonov <jirisimonov@gmail.com>
2022-12-25 16:21:50 +0300

    refactor: format document

--- src/components/cart/cart-promo.ts
+++ src/components/cart/cart-promo.ts
@@ -4,32 +4,31 @@ import { BaseComponent } from '../elements/base-component';
 import { Button } from '../elements/button';
 
 export class PromoForm extends BaseComponent {
-  private promoBtn: Button;
-
+  private promoBtn = new Button({
+    className: 'promo__submit-btn',
+    text: 'Применить',
+  });
   private promoField = new FormField({
     className: 'promo',
     type: 'text',
     placeholder: 'KEK',
-    text: `Промокоды: ${DB.cart.promo.available.join(', ')}` });
+    text: `Промокоды: ${DB.cart.promo.available.join(', ')}`
+  });
 
   constructor() {
     super({ tag: 'form', className: 'promo__form' });
-    this.promoBtn = new Button({
-      className: 'promo__submit-btn',
-      text: 'Применить',
-      onclick: () => {
-        if (DB.cart.promo.isValid(this.promoField.getInputNode().value)) {
-          DB.cart.promo.add(this.promoField.getInputNode().value);
-          this.promoField.getInputNode().value = '';
-          this.node.removeChild(this.promoBtn.getNode());
-        }
-        return false;
-      },
-    });
+    this.promoBtn.onclick = () => {
+      if (DB.cart.promo.isValid(this.promoField.getInputNode().value)) {
+        DB.cart.promo.add(this.promoField.getInputNode().value);
+        this.promoField.getInputNode().value = '';
+        this.node.removeChild(this.promoBtn.getNode());
+      }
+      return false;
+    }
     this.promoField.getInputNode().oninput = () => {
-      if (DB.cart.promo.isValid(this.promoField.getInputNode().value)) 
-        this.appendEl(this.promoBtn)
-       else 
+      if (DB.cart.promo.isValid(this.promoField.getInputNode().value))
+        this.appendEl(this.promoBtn);
+      else
         if (this.node.children.length > 1) this.node.removeChild(this.promoBtn.getNode());
     }
     this.appendEl(this.promoField);
