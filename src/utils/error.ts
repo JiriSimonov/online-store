ad8fb75590b8cbbbd901e68500cb3685fe7cee3a
JiriSimonov <jirisimonov@gmail.com>
2022-12-26 00:37:17 +0300

    fix: error page class

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,13 +1,8 @@
 import { BaseComponent } from "../components/elements/base-component";
 
 export class Error extends BaseComponent {
-  private title: BaseComponent;
+  private title = new BaseComponent({ tag: 'h1', className: 'error', text: 'Страница не найдена', parent: this.node });
   constructor() {
     super({ className: 'container' });
-    this.title = new BaseComponent({ tag: 'h1', className: 'error', text: 'Страница не найдена' });
-  }
-
-  render() {
-    this.appendEl(this.title);
   }
 }
