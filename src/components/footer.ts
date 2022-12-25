6dbc1ddfbec294663b935ab671ce3f07d4f1fd82
JiriSimonov <jirisimonov@gmail.com>
2022-12-26 00:46:24 +0300

    refactor: footer class

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -2,53 +2,39 @@ import { Anchor } from './elements/anchor';
 import { BaseComponent } from './elements/base-component';
 
 export class Footer extends BaseComponent {
-  private wrapper: BaseComponent;
+  private wrapper = new BaseComponent({ className: 'container', parent: this.node });
 
-  private contentWrapper: BaseComponent;
+  private contentWrapper = new BaseComponent({ className: 'footer__wrapper', parent: this.wrapper.getNode() });
 
-  private year: BaseComponent;
+  private linkWrapper = new BaseComponent({ className: 'footer__links', parent: this.contentWrapper.getNode() });
 
-  private logo: BaseComponent;
+  private linkOne = new Anchor({
+    className: 'footer__link',
+    text: '@EternalRival',
+    href: 'https://github.com/EternalRival',
+    target: '_blank',
+    parent: this.linkWrapper.getNode(),
+  });
 
-  private linkWrapper: BaseComponent;
+  private linkTwo = new Anchor({
+    className: 'footer__link',
+    text: '@JiriSimonov',
+    href: 'https://github.com/JiriSimonov',
+    target: '_blank',
+    parent: this.linkWrapper.getNode(),
+  });
 
-  private linkOne: BaseComponent;
+  private year = new BaseComponent({ className: 'footer__year', text: '2022', parent: this.contentWrapper.getNode() });
 
-  private linkTwo: BaseComponent;
+  private logo = new Anchor({
+    className: 'footer__logo',
+    href: 'https://rs.school/js/',
+    target: '_blank',
+    parent: this.contentWrapper.getNode(),
+  });
 
   constructor() {
     super({ tag: 'footer', className: 'footer' });
-    this.wrapper = new BaseComponent({ className: 'container' });
-    this.contentWrapper = new BaseComponent({ className: 'footer__wrapper' });
-    this.year = new BaseComponent({ className: 'footer__year', text: '2022' });
-    this.logo = new Anchor({
-      tag: 'a',
-      className: 'footer__logo',
-      href: 'https://rs.school/js/',
-      target: '_blank',
-    });
     this.logo.getNode().setAttribute('aria-label', 'Курсы RSS JS/FE');
-    this.linkWrapper = new BaseComponent({ className: 'footer__links' });
-    this.linkOne = new Anchor({
-      tag: 'a',
-      className: 'footer__link',
-      text: '@EternalRival',
-      href: 'https://github.com/EternalRival',
-      target: '_blank',
-    });
-    this.linkTwo = new Anchor({
-      tag: 'a',
-      className: 'footer__link',
-      text: '@JiriSimonov',
-      href: 'https://github.com/JiriSimonov',
-      target: '_blank',
-    });
-  }
-
-  render() {
-    this.wrapper.appendEl(this.contentWrapper);
-    this.linkWrapper.appendEl([this.linkOne, this.linkTwo]);
-    this.contentWrapper.appendEl([this.linkWrapper, this.year, this.logo]);
-    this.appendEl(this.wrapper);
   }
 }
