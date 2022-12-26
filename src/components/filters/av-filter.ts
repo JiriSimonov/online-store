b96487ec3f18c89a763c70c7c97ffdeb6951d74c
JiriSimonov <jirisimonov@gmail.com>
2022-12-26 13:56:01 +0300

    feat: add form fields

--- src/components/filters/av-filter.ts
+++ src/components/filters/av-filter.ts
@@ -1,17 +1,22 @@
+import { FormField } from '../elements/form-field';
+import { DB } from '../../services/db/database';
 import { BaseComponent } from '../elements/base-component';
 import { Filter } from './filter';
 import { Button } from '../elements/button';
 
 export class AvFilter extends Filter {
-  private filterWrapper: BaseComponent;
+  private filterWrapper = new BaseComponent({ className: 'filter__wrapper', parent: this.node });
 
   private all: Button;
 
   private inStock: Button;
 
+  private items = [...DB.getVariants('available')]
+  .map((item) => new FormField({ className: 'filter', type: 'checkbox', text: item as string }));
+
   constructor() {
     super('Наличие');
-    this.filterWrapper = new BaseComponent({ className: 'filter__wrapper', parent: this.node });
+    this.appendEl(this.items);
     this.all = new Button({
       className: 'filter__btn active',
       text: 'Всё',
