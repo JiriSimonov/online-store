de7a8e7b39f3f68bb9e91e4077ed2229a83d72d6
EternalRival <eternalrival8@gmail.com>
2022-12-27 10:43:36 +0300

    feat: add price/quantity filtration refactor: simplify filter methods

--- src/components/filters/price-filter.ts
+++ src/components/filters/price-filter.ts
@@ -3,7 +3,6 @@ import { Filter } from './filter';
 import { DualSlider } from '../elements/dual-slider';
 import { BaseComponent } from '../elements/base-component';
 
-
 export class PriceFilter extends Filter {
   private filterWrapper = new BaseComponent({ className: 'filter__wrapper', parent: this.node });
 
@@ -11,13 +10,17 @@ export class PriceFilter extends Filter {
 
   constructor() {
     super('Цена');
-    const filteredMax = DB.filter.list.reduce((max, kb) => (kb.priceMax > max ? kb.priceMax : max), 0);
-    const filteredMin = DB.filter.list.reduce((min, kb) => (kb.priceMin < min ? kb.priceMin : min), filteredMax);
+    const filteredMax = DB.keyboards.reduce((max, kb) => (kb.priceMax > max ? kb.priceMax : max), 0);
+    const filteredMin = DB.keyboards.reduce((min, kb) => (kb.priceMin < min ? kb.priceMin : min), filteredMax);
     this.slider = new DualSlider(filteredMin, filteredMax, 100, 1000);
-    const [minNum, maxNum] = this.slider.getNumbersNodes();
-    const [minRange, maxRange] = this.slider.getRangesNodes();
-    [minNum, minRange].forEach((v) => v.addEventListener('change', () => DB.filter.setParam('minPrice', v.value)));
-    [maxNum, maxRange].forEach((v) => v.addEventListener('change', () => DB.filter.setParam('maxPrice', v.value)));
     this.filterWrapper.appendEl(this.slider);
+
+    const [minNum, maxNum, minRange, maxRange] = [...this.slider.getNumbersNodes(), ...this.slider.getRangesNodes()];
+    [minNum, minRange].forEach((item) =>
+      item.addEventListener('change', () => DB.filter.clear('minPrice').add('minPrice', item.value)),
+    );
+    [maxNum, maxRange].forEach((item) =>
+      item.addEventListener('change', () => DB.filter.clear('maxPrice').add('maxPrice', item.value)),
+    );
   }
 }
--- src/components/filters/quantity-filter.ts
+++ src/components/filters/quantity-filter.ts
@@ -3,7 +3,6 @@ import { Filter } from './filter';
 import { DualSlider } from '../elements/dual-slider';
 import { BaseComponent } from '../elements/base-component';
 
-
 export class QuantityFilter extends Filter {
   private filterWrapper = new BaseComponent({ className: 'filter__wrapper', parent: this.node });
 
@@ -11,12 +10,16 @@ export class QuantityFilter extends Filter {
 
   constructor() {
     super('Остаток на складе');
-    const filteredMax = DB.filter.list.reduce((max, kb) => (kb.sumQuantity > max ? kb.sumQuantity : max), 0);
+    const filteredMax = DB.keyboards.reduce((max, kb) => (kb.sumQuantity > max ? kb.sumQuantity : max), 0);
     this.slider = new DualSlider(0, filteredMax, 6, 2);
-    const [minNum, maxNum] = this.slider.getNumbersNodes();
-    const [minRange, maxRange] = this.slider.getRangesNodes();
-    [minNum, minRange].forEach((v) => v.addEventListener('change', () => DB.filter.setParam('minQuantity', v.value)));
-    [maxNum, maxRange].forEach((v) => v.addEventListener('change', () => DB.filter.setParam('maxQuantity', v.value)));
     this.filterWrapper.appendEl(this.slider);
+
+    const [minNum, maxNum, minRange, maxRange] = [...this.slider.getNumbersNodes(), ...this.slider.getRangesNodes()];
+    [minNum, minRange].forEach((item) =>
+      item.addEventListener('change', () => DB.filter.clear('minQuantity').add('minQuantity', item.value)),
+    );
+    [maxNum, maxRange].forEach((item) =>
+      item.addEventListener('change', () => DB.filter.clear('maxQuantity').add('maxQuantity', item.value)),
+    );
   }
 }
