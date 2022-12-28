4386d4f0cb555bd14d701fe0ebbd3bb5ee078c70
JiriSimonov <jirisimonov@gmail.com>
2022-12-28 20:40:58 +0300

    feat: add sort buttons

--- src/components/filters/sort-filter.ts
+++ src/components/filters/sort-filter.ts
@@ -1,4 +1,5 @@
 import { Filter } from './filter';
+import { Button } from '../elements/button';
 import { BaseComponent } from '../elements/base-component';
 import { FormField } from '../elements/form-field';
 import { DB } from '../../services/db/database';
@@ -7,43 +8,71 @@ export class SortFilter extends Filter {
   private filterWrapper = new BaseComponent({ className: 'filter__wrapper', parent: this.node });
   private nameFilter = new FormField({
     className: 'filter',
-    type: 'checkbox',
+    type: 'radio',
+    name: 'filter-sort',
     modificator: 'sort',
     text: 'Алфавиту',
   });
   private priceFilter = new FormField({
     className: 'filter',
-    type: 'checkbox',
+    type: 'radio',
+    name: 'filter-sort',
     modificator: 'sort',
-    text: 'Возрастанию / убыванию цены',
+    text: 'Цене',
   });
   private inStockFilter = new FormField({
     className: 'filter',
-    type: 'checkbox',
+    type: 'radio',
+    name: 'filter-sort',
     modificator: 'sort',
     text: 'Остатку на складе',
   });
+  private filterControls = new BaseComponent({
+    className: 'filter__wrapper filter__wrapper_controls',
+    parent: this.node,
+  });
+  private ascendingFilter = new FormField({
+    className: 'filter',
+    type: 'radio',
+    name: 'filter-sort-direction',
+    modificator: 'sort',
+    text: 'Возрастанию',
+  });
+  private descendingFilter = new FormField({
+    className: 'filter',
+    type: 'radio',
+    name: 'filter-sort-direction',
+    modificator: 'sort',
+    text: 'Убыванию',
+  });
+  private resetSort = new Button({
+    className: 'filter__clear',
+    text: 'Очистить сортировку',
+    onclick: () => {
+      DB.filter.setParam('sortType').setParam('sortDirection');
+      this.uncheckAll();
+    },
+    parent: this.node,
+  });
+
 
   constructor() {
     super('Сортировать по');
-    this.filterWrapper.appendEl([this.nameFilter, this.priceFilter, this.inStockFilter]);
-    this.perepisat(); //? метод написан чисто под тестирование онкликов, чтоб не трогать остальное
+    this.nameFilter.getInputNode().onchange = () => DB.filter.setParam('sortType', 'title');
+    this.priceFilter.getInputNode().onchange = () => DB.filter.setParam('sortType', 'priceMin');
+    this.inStockFilter.getInputNode().onchange = () => DB.filter.setParam('sortType', 'sumQuantity');
+    this.ascendingFilter.getInputNode().onchange = () => DB.filter.setParam('sortDirection', 'ascending');
+    this.descendingFilter.getInputNode().onchange = () => DB.filter.setParam('sortDirection', 'descending');
+    this.filterWrapper.appendEl([
+      this.nameFilter,
+      this.priceFilter,
+      this.inStockFilter]
+    );
+    this.filterControls.appendEl([this.ascendingFilter,this.descendingFilter]);
   }
 
-  perepisat() {
-    const buttons = ['🅰', '💰', '📦', '☝', '👇', '🧼'].map((name, index) => {
-      const btn = document.createElement('button');
-      btn.style.width = index > 2 ? '32%' : '30%';
-      btn.style.margin = '.2em';
-      btn.textContent = name;
-      return btn;
-    });
-    this.node.append(...buttons);
-    buttons[0].onclick = () => DB.filter.setParam('sortType', 'title');
-    buttons[1].onclick = () => DB.filter.setParam('sortType', 'priceMin');
-    buttons[2].onclick = () => DB.filter.setParam('sortType', 'sumQuantity');
-    buttons[3].onclick = () => DB.filter.setParam('sortDirection', 'ascending');
-    buttons[4].onclick = () => DB.filter.setParam('sortDirection', 'descending');
-    buttons[5].onclick = () => DB.filter.setParam('sortType').setParam('sortDirection');
+  uncheckAll() {
+    [this.nameFilter, this.priceFilter, this.inStockFilter, this.ascendingFilter, this.descendingFilter]
+      .forEach((item) => {Object.assign(item, { checked: false })});
   }
 }
