2925c30d6b36b56d31c266e21f93124ec2fed3f2
EternalRival <eternalrival8@gmail.com>
2023-01-05 23:21:34 +0300

    refactor: change input component

--- src/components/elements/input-component.ts
+++ src/components/elements/input-component.ts
@@ -3,9 +3,11 @@ import { Component, ComponentProps } from './component';
 export class Input extends Component<HTMLInputElement> {
   constructor(props?: ComponentProps<HTMLInputElement>) {
     super({ ...props, tag: 'input' });
+
+    this.node.required = true;
+
     if (this.node.type === 'number')
       this.node.onkeydown = (e) => {
-        // if (e.target instanceof HTMLInputElement && ['e', 'E', '-', '+', '.', ','].includes(e.key)) e.preventDefault();
         if (['e', 'E', '-', '+', '.', ','].includes(e.key)) e.preventDefault();
       };
   }
--- src/components/elements/input.ts
+++ src/components/elements/input.ts
@@ -1,25 +1,14 @@
-import { BaseComponent } from './base-component';
-import { InputProps } from '../../interfaces/interfaces';
+import { Component, ComponentProps } from './component';
 
-export class Input extends BaseComponent<HTMLInputElement> {
-  constructor(props: InputProps) {
-    super(Object.assign(props, { tag: 'input' }));
-    const { value, type, placeholder, pattern, min, max, name, step, checked } = props;
-    const { node } = this;
-    if (value) node.setAttribute('value', value);
-    if (type) node.setAttribute('type', type);
-    if (type === 'number')
+export class Input extends Component<HTMLInputElement> {
+  constructor(props?: ComponentProps<HTMLInputElement>) {
+    super({ ...props, tag: 'input' });
+
+    this.node.required = true;
+
+    if (this.node.type === 'number')
       this.node.onkeydown = (e) => {
-        if (e.target && e.target instanceof HTMLInputElement && ['e', 'E', '-', '+', '.', ','].includes(e.key))
-          e.preventDefault();
+        if (['e', 'E', '-', '+', '.', ','].includes(e.key)) e.preventDefault();
       };
-    if (placeholder) node.setAttribute('placeholder', placeholder);
-    if (pattern) node.setAttribute('pattern', pattern);
-    if (min)node.setAttribute('min', min);
-    if (max) node.setAttribute('max', max);
-    if (name) node.setAttribute('name', name);
-    if (step) node.setAttribute('step', step);
-    if (checked) node.checked = checked;
-    node.required = true;
   }
 }
