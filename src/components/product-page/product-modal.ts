3a0fff9b4726cb5b27f1f2f0169095649ee24e99
JiriSimonov <jirisimonov@gmail.com>
2023-01-05 07:57:22 +0300

    fix: product page modal

--- src/components/product-page/product-modal.ts
+++ src/components/product-page/product-modal.ts
@@ -3,11 +3,21 @@ import { Button } from '../elements/button';
 
 export class ProductModal extends BaseComponent {
   private modalContent = new BaseComponent({ className: 'product__content', parent: this.node });
-  private closeModal = new Button({ className: 'modal__close', onclick: () => this.destroy(), parent: this.node });
+  private closeModal = new Button({
+    className: 'modal__close',
+    onclick: () => {
+      this.destroy();
+      window.scrollTo(0,0);
+    },
+    parent: this.node,
+  });
 
   constructor(bg: string) {
     super({ className: 'product__modal' });
     this.modalContent.setStyleAttr(['backgroundImage', bg]);
-    this.node.onclick = () => this.destroy();
+    this.node.onclick = () => {
+      this.destroy();
+      window.scrollTo(0,0);
+    };
   }
 }
