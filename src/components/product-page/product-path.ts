e08403c6a472e234aa68ef6197349d447e856dff
JiriSimonov <jirisimonov@gmail.com>
2023-01-08 13:23:43 +0300

    fix: product path

--- src/components/product-page/product-path.ts
+++ src/components/product-page/product-path.ts
@@ -1,20 +1,32 @@
+import { Emitter } from '../../services/emitter';
 import { Component } from '../elements/base-component';
+import { DB } from '../../services/db/database';
+import { Keyboard } from '../../services/db/keyboard';
 
 export class ProductPath extends Component {
   private store = new Component({
     tag: 'li',
     className: 'product__path-item',
-    textContent: 'store',
+    textContent: 'Клавиатуры',
     parent: this,
     onclick: () => {
       window.location.hash = '/store';
     },
   });
-  private brands = new Component({ tag: 'li', className: 'product__path-item', textContent: 'бренды', parent: this });
+  private brands = new Component({
+    tag: 'li',
+    className: 'product__path-item',
+    onclick: () => {
+      window.location.hash = '/store';
+    },
+    parent: this,
+  });
   private product = new Component({ tag: 'li', className: 'product__path-item', parent: this });
 
-  constructor(productName: string) {
+  constructor(productProps: Keyboard) {
     super({ tag: 'ul', className: 'product__path' });
-    this.product.text = productName;
+    [this.brands.text] = productProps.manufacturer;
+    if (!productProps.manufacturer.length) this.brands.text = 'Brand';
+    this.product.text = productProps.title;
   }
 }
